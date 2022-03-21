c17761ef0b5a621b7c035a20de2f7c8d185c28fb
Kirill <gujik228@gmail.com>
2022-03-21 10:03:19 +0700

    test

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,13 +1,13 @@
+#include "geometry.h"
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include "geometry.h"
 
 int main(void)
 {
-	test();
+    test();
 
     int n_str = 50;
     int n_re = 10;
@@ -38,7 +38,9 @@ int main(void)
                    y,
                    r);
 
-			printf("Длина окружности: %.3f, площадь: %.3f", 2 * 3.14 * r, r * r * 3.14);
+            printf("Длина окружности: %.3f, площадь: %.3f",
+                   2 * 3.14 * r,
+                   r * r * 3.14);
 
         } else if (strcmp(re, "triangle") == 0) {
             float x1, y1, x2, y2, x3, y3, x4, y4, a, b, c;
@@ -61,14 +63,14 @@ int main(void)
                 continue;
             }
 
-			if (x1 != x4 || y1 != y4) {
+            if (x1 != x4 || y1 != y4) {
                 printf("Invalid input!\n");
                 continue;
-			}
+            }
 
-			a = sqrt(pow((x2 - x3), 2) + pow((y2 - y3), 2));
-			b = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
-			c = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+            a = sqrt(pow((x2 - x3), 2) + pow((y2 - y3), 2));
+            b = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
+            c = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
 
             printf("Это %s, координаты: x1 = %.3f y1 = %.3f, x2 = %.3f y2 = "
                    "%.3f, x3 = %.3f y3 = %.3f, x4 = %.3f y4 = %.3f\n",
@@ -82,7 +84,10 @@ int main(void)
                    x4,
                    y4);
 
-			printf("Периметр: %.3f, площадь: %.3f", a + b + c, fabs((x1 * (y2 - y3) + x2 * (y1 - y3) + x3 * (y1 - y2)) / 2));
+            printf("Периметр: %.3f, площадь: %.3f",
+                   a + b + c,
+                   fabs((x1 * (y2 - y3) + x2 * (y1 - y3) + x3 * (y1 - y2))
+                        / 2));
 
         } else
             printf("Invalid input!\n");
